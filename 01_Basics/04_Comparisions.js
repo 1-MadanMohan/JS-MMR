9d2aca20e00662c897fc3aaf3f2feca5135c4918
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-03-17 05:38:59 +0000

    datatype summary

---
+++ 01_Basics/04_Comparisions.js
@@ -0,0 +1,20 @@
+console.log(2>1)
+console.log(2==1)
+console.log(2!=1)
+// Trivial ones
+
+console.log("2">1)
+console.log("02">1)
+
+//Comparators and Equalityy works differently in js
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+//== and === difference
+
---
+++ 01_Basics/05_DataTypes_Summary.js
@@ -0,0 +1,23 @@
+//Two categorizations of Datatypes
+// 1.Primitive
+// String,Number,Boolean,null,undefined,,Symbol,BigInt
+
+// 2.Non Primitive or Reference Type
+// Array,Objects,Functions
+
+//const,var,let
+//Static or Dynamic ?
+
+
+//Array
+
+const Actors = ["Hunt","Bond","Wick"]
+//Object
+let myObj= {
+    name: "MMR",
+    age: 22,
+}
+//Function
+const myFunction = function(){
+    console.log("Hello")
+}
