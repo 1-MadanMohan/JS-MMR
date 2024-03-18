6714f86dfa48593322f0af7e180c1e523ebfd43a
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-03-18 03:58:52 +0000

    Num and Math

---
+++ 01_Basics/07_numAndMath.js
@@ -0,0 +1,34 @@
+// const score = 400
+// console.log(score)
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// const otherNumber =23.8966
+// console.log(otherNumber.toPrecision(3))
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//Maths
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(9,3,5,53));
+
+
+// console.log(Math.random());
+// //random one beteeen 0 and 1
+const min = 10
+const max = 20
+
+// random value between 10 and 20
+ console.log(Math.floor(Math.random()* (max-min+1)) +min);
