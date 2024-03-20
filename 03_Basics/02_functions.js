1958f53f752f42f654361400d8ed2dba0f6409ee
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-03-20 14:29:53 +0000

    function

---
+++ 03_Basics/02_functions.js
@@ -0,0 +1,14 @@
+function calculteCardPrice(num1){
+return num1;
+}
+console.log(calculteCardPrice(3));
+
+function calculteCardPrice1(val1,val2, ...num){// rest operator(...)
+ return num;
+}
+console.log(calculteCardPrice1(200,200,300,1200));
+
+function calculteCardPrice2(...num){
+    return num;
+}
+console.log(calculteCardPrice2(200,200,300,1200));
