602a34c437e78d60d1126f3b41eb2459bcdbb282
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-03-20 13:02:42 +0000

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,14 @@
+function write(){
+    console.log("R")
+    console.log("E");
+    console.log("D");
+}
+write();
+
+
+function addNum(num1,num2){
+console.log(num1 +num2);
+
+}
+addNum(3,4)
+addNum(3,"a")
