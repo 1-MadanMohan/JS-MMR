61aa7606410a48ed71fcc68baae2d2d7afbcd55d
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-03-28 18:12:55 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+//if statements
+// Remember the difference between if-else, if after statement
+// ==,===,!=,<,>, operators
+console.log(3==2);
+console.log(3===2+1);
+console.log(3=='3');
+console.log(3==='3');
+let balance = 600
+if(balance >500) console.log("Yeah");// has implicit scope which should be in one line
+
+if(balance>50) console.log("line 1"),
+console.log("line 2");
