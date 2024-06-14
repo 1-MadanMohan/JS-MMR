2da54ecefe632c4c468121225766f7f01e07a19b
1-MadanMohan <119772090+1-MadanMohan@users.noreply.github.com>
2024-06-14 12:38:24 +0000

    project 2

--- 07_Projects/project2/req.js
+++ 07_Projects/project2/req.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form')
 //this use case will give u empty value
 // const height = parseInt(document.querySelector('#height').value)
 form.addEventListener('submit',function(e){
-    e.preventDefault()
+    e.preventDefault();
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
